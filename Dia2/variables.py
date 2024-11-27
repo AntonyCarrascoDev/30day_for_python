74deaea86a95243f382c9c015381e90bcd82373c
Antony Carrasco <antony.carrascoa@gmail.com>
2024-11-26 22:28:27 -0500

    Dia 2 - completo

---
+++ Dia2/variables.py
@@ -0,0 +1,36 @@
+#Ejercicios: Nivel 1
+#Dentro de 30DaysOfPython crea una carpeta llamada day_2. Dentro de esta carpeta crea un archivo llamado variables.py
+#Escribe un comentario en Python que diga 'Día 2: 30 días de programación en Python'
+#Día 2: 30 días de programación en Python
+#Declarar una variable de nombre y asignarle un valor
+first_name = 'Antony'
+
+#Declarar una variable de apellido y asignarle un valor
+last_name = 'Carrasco'
+
+#Declarar una variable con nombre completo y asignarle un valor
+complet_name = 'Antony Carrasco'
+
+#Declarar una variable de país y asignarle un valor
+name_contry = 'Perú'
+
+#Declarar una variable de ciudad y asignarle un valor
+name_city = 'Chiclayo'
+
+#Declarar una variable de edad y asignarle un valor
+my_age = 28
+
+#Declarar una variable de año y asignarle un valor
+year = 2024
+
+#Declarar una variable is_married y asignarle un valor
+is_married = False
+
+#Declarar una variable is_true y asignarle un valor
+is_true = True
+
+#Declarar una variable is_light_on y asignarle un valor
+is_light_on = False
+
+#Declarar múltiples variables en una línea
+a,b,c = 24,25,27
